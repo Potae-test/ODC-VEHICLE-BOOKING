da4bd4ceda321e4fbb54838403ccc912f15c8ec2
Potae-test <kooysky@gmail.com>
2026-05-08 22:15:16 +0700

    deploy to cloud

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
-// const API_BASE_URL = "https://odc-vehicle-api.kooysky.workers.dev";
-const API_BASE_URL = "http://localhost:8787";
+const API_BASE_URL = "https://odc-vehicle-api.kooysky.workers.dev";
+// const API_BASE_URL = "http://localhost:8787";
 // ---------------------
 // VEHICLES
 // ---------------------
